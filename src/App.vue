359c3fcc3a3327b6863f3c08a94a8a8c760cc5d9
fharahap85 <82890658+fharahap85@users.noreply.github.com>
2026-04-28 08:49:03 +0700

    Feat: Tambah integrasi Firebase Cloud Messaging untuk ambil token dan handle notifikasi

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,68 @@
 <template>
   <div class="max-w-full">
     <Navbar v-if="!$route.meta.hideNavbarFooter" />
-
     <router-view />
 
     <Footer v-if="!$route.meta.hideNavbarFooter" />
+    <!-- <div v-if="token">
+      <p>Firebase Token: {{ token }}</p>
+    </div> -->
   </div>
 </template>
 
 <script>
-import Footer from "./views/Footer.vue"; // Pastikan path ini benar
-import Navbar from "./views/Navbar.vue"; // Pastikan path ini benar
+import Footer from "./views/Footer.vue";
+import Navbar from "./views/Navbar.vue";
+import { initializeApp } from "firebase/app";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD5yt182xOHeUnxa06vz1SwF4cxQJE49KU",
+  authDomain: "notifikasi-74c94.firebaseapp.com",
+  projectId: "notifikasi-74c94",
+  storageBucket: "notifikasi-74c94.firebasestorage.app",
+  messagingSenderId: "632621079630",
+  appId: "1:632621079630:web:bcffc7d2e4b5a481cfb71f",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const messaging = getMessaging();
 
 export default {
   components: {
     Navbar,
     Footer,
   },
-  // Anda tidak perlu data atau computed property di sini jika hanya menggunakan $route.meta
+  data() {
+    return {
+      token: "", // Menyimpan token
+    };
+  },
+  created() {
+    // Mendapatkan token dan menyimpannya di data komponen
+    getToken(messaging, {
+      vapidKey:
+        "BOYhj6vysb_YmJ0ozF8wDNXN_XjNQuVYdV8JumbJXOXVrNXBs3pmDym-gr9vLnDNr583Lkuw9quOXVEsSnu_7mQ",
+    })
+      .then((currentToken) => {
+        if (currentToken) {
+          this.token = currentToken; // Menyimpan token ke dalam data
+          console.log("Token is : ", currentToken);
+        } else {
+          console.log(
+            "No registration token available. Request permission to generate one."
+          );
+        }
+      })
+      .catch((err) => {
+        console.log("An error occurred while retrieving token. ", err);
+      });
+
+    // Mendengarkan pesan masuk
+    onMessage(messaging, (payload) => {
+      console.log("Message received. ", payload);
+    });
+  },
 };
 </script>
-
-<style>
-/* Gaya global untuk aplikasi Anda, jika ada */
-</style>
