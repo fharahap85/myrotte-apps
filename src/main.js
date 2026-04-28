d8dc1bfb72fa0e180c6da0f12a24be0db7b5cb91
fharahap85 <82890658+fharahap85@users.noreply.github.com>
2026-04-28 08:50:15 +0700

    Feat: tambah integrasi OneSignal untuk push notification di Vue app

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
+import OneSignalVuePlugin from "@onesignal/onesignal-vue3";
 
 import "./assets/main.css";
 
@@ -11,4 +12,12 @@ const app = createApp(App);
 
 app.use(router);
 app.use(Toast);
+app.use(OneSignalVuePlugin, {
+  appId: "7aee32c7-ed71-4984-ad52-a3c70852085c", // Ganti dengan App ID dari OneSignal
+  allowLocalhostAsSecureOrigin: true, // Untuk testing di localhost
+  notifyButton: {
+    enable: true, // Aktifkan tombol notifikasi
+  },
+});
+
 app.mount("#app");
